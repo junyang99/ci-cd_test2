1ca4a1513787937a8e89174ffd0cd406f089739d
junyang99 <87479326+junyang99@users.noreply.github.com>
2023-11-07 04:47:40 +0800

    Update view_staff.py

--- utils/view_staff.py
+++ utils/view_staff.py
@@ -6,8 +6,13 @@ from view_access_control import Access_Control
 
 app = Flask(__name__)
 
-# Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/HR Portal'
+db_user = os.getenv('DB_USER', 'root')  # default to 'root' if not set
+db_pass = os.getenv('DB_PASSWORD', 'root')  # default to 'root' if not set
+db_host = os.getenv('DB_HOST', '127.0.0.1')  # default to 'localhost' if not set
+db_name = os.getenv('DB_NAME', 'HR Portal')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_pass}@{db_host}:3306/{db_name}'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/HR Portal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
