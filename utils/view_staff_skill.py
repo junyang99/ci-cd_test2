08c1d4073c185446019c0dbddd5d00bd65ed86d7
junyang99 <87479326+junyang99@users.noreply.github.com>
2023-11-07 03:30:36 +0800

    Update view_staff_skill.py

--- utils/view_staff_skill.py
+++ utils/view_staff_skill.py
@@ -15,7 +15,7 @@ db_host = os.environ.get('MYSQL_HOST', 'localhost')  # Default to 'localhost' if
 db_port = os.environ.get('MYSQL_PORT', '3306')  # Default to '3306' if not set
 db_name = os.environ.get('MYSQL_DATABASE', 'HR Portal')  # Default to 'HR_Portal' if not set
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'fallback_connection_string')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
